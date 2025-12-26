3122f1c99d359dd82527a3798ea208df7909d702
Hasanboy1295 <jabborovhasan853@gmail.com>
2025-12-26 18:38:21 +0900

    feat: build project standards

--- apps/nestar-api/src/app.module.ts
+++ apps/nestar-api/src/app.module.ts
@@ -5,14 +5,18 @@ import { ConfigModule } from '@nestjs/config';
 import { GraphQLModule} from '@nestjs/graphql';
 import { ApolloDriver } from '@nestjs/apollo';
 import { AppResolver } from './app.resolver';
-
+import { DatabaseModule } from './database/database.module';
+ 
 @Module({
-	imports: [ConfigModule.forRoot(), GraphQLModule.forRoot({
+	imports: [
+    ConfigModule.forRoot(),
+    GraphQLModule.forRoot({
     driver: ApolloDriver,
     playground: true,
     uploads: false,
     autoSchemaFile: 'schema.gql',
-  })],
+  }),
+    DatabaseModule],
 	controllers: [AppController],
 	providers: [AppService, AppResolver],
 })
---
+++ apps/nestar-api/src/libs/enums/common.enum.ts
@@ -0,0 +1,19 @@
+export enum Message {
+  SOMETHING_WENT_WRONG = "Something went wrong!",
+  NO_DATA_FOUND = "Flodata is found!",
+  CREATE_FAILED = "Create is failed!",
+  UPDATE_FAILED = "Update is failed!",
+  REMOVE_FAILED = "Remove is failed!",
+  UPLOAD_FAILED = "Upload is failed!",
+  BAD_REQUEST = "Bad request!",
+
+  NO_MEMBER_NICK = "No member with this member nick!",
+  BLOCKED_USER = "You have been blocked",
+  WRONG_PASSWORD = "Wrong Password, try again!",
+  NOT_AUTHENTICATION = "You are not authenticated, Please login first!",
+  TOKEN_NOT_EXIST  = "Barer token not provided!",
+  ONLY_SPECIFIC_ROLES_ALLOWED = "Allowed only for members with specific roles!",
+ NOT_ALLOWED_REQUEST = "Not allowed request!",
+ PROVIDE_ALLOWED_FORMAT = "Please provide jpeg, jpg or  png  images!",
+ SELF_SUBSCRIPTION_DEFINED = "Self subscription is defined!",
+}
