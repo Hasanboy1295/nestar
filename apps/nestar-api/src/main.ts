63c50d35fc9bf24c3a1dfd4a6bb538461d0d9370
Hasanboy1295 <jabborovhasan853@gmail.com>
2025-12-28 18:05:24 +0900

    fix: integrate pipe valedaation

--- apps/nestar-api/src/main.ts
+++ apps/nestar-api/src/main.ts
@@ -1,8 +1,10 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {//1 bootstrap funksiyasi ishga tushadi
   const app = await NestFactory.create(AppModule);//meth chaqirib  appmoduleni arg ber  app=> obj nestjs+ express qorishmasi 
+ app.useGlobalPipes(new ValidationPipe());
   await app.listen(process.env.PORT_API ?? 3000);//app.listen ga tegshli 
 }
 bootstrap();
