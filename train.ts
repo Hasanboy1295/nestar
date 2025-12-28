82cc471cae91b9a561983afc3b64655d0ce946e6
Hasanboy1295 <jabborovhasan853@gmail.com>
2025-12-28 09:17:56 +0900

    feat: TASK ZM

--- train.ts
+++ train.ts
@@ -1,3 +1,34 @@
+
+
+
+
+
+
+
+
+
+
+
+// TASK ZM:
+
+// Shunday function yozing, va bu function parametr
+// sifatida raqamlarni qabul qilsin. Bu function qabul qilingan
+// raqamlarni orqasiga o'girib qaytarsin
+
+// MASALAN: reverseInteger(123456789); return 987654321;
+
+// Yuqoridagi misolda, function kiritilgan raqamlarni orqasiga
+// o'girib (reverse) qilib qaytarmoqda.
+
+function reverseInteger(num) {
+  return Number(
+    num.toString().split('').reverse().join('')
+  );
+}
+
+console.log(reverseInteger(123456789)); 
+
+
 // TASK ZK:
 // Shunday function yozing, bu function har bir soniyada bir marotaba
 // console'ga 1'dan 5'gacha bo'lgan raqamlarni chop etsin va
@@ -19,3 +50,5 @@ function printNumbers() {
 }
 
 printNumbers();
+
+
