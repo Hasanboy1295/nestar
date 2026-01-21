a1602c22b5fa9e008ec4dddc84e8786855e1b6b6
Hasanboy1295 <jabborovhasan853@gmail.com>
2026-01-22 06:21:55 +0900

    feat: develop getVisited GraphQL APIs

--- apps/nestar-api/src/libs/config.ts
+++ apps/nestar-api/src/libs/config.ts
@@ -28,43 +28,7 @@ export const shapeIntoMongoObjectId = (target: any) => {
 	return typeof target === 'string' ? new ObjectId(target) : target;
 };
 
-//auth bolgan member.      //propniId            //hosil bolgan property id ni qabul qilish
-// export const lookupAuthMemberLiked = (memberId: T, targetRefId: string = '$_id') => {
-// 	//idRefId mavjud bolmasa shu qiymatni oladi
-// 	return {
-// 		$lookup: {
-// 			//localvariable
-// 			from: 'likes', //likees collectionda izledi
-// 			let: {
-// 				//likes ni ichiga kirib shuni ichidan qidirishini aytyappiz (search)
-// 				localLikeRefId: targetRefId, //"$_id" bu shunde degani
-// 				localMemberId: memberId,
-// 				localMyFavorite: true,
-// 			},
-// 			//pipeline
-// 			pipeline: [
-// 				{
-// 					$match: {
-// 						$expr: {
-// 							//aynan nimalarni solishtirishimizni belgilaypamiz
-// 							$and: [{ $eq: ['$likeRefId', '$$localLikeRefId'] }, { $eq: ['$memberId', '$$localMemberId'] }], //ikkalasin teng holatini izla
-// 							//memberId ni localLikeRefId ga teng bolgan holatin izla
-// 						},
-// 					},
-// 				},
-// 				{
-// 					$project: {
-// 						_id: 0,
-// 						memberId: 1,
-// 						likeRefId: 1,
-// 						myFavorite: '$$localMyFavorite',
-// 					},
-// 				},
-// 			],
-// 			as: 'meLiked', //pipeline ni natijasini qandayb saqlamoqchimiz (me liked nomi bn saqlashimiz ker)
-// 		},
-// 	};
-// };
+
 
 export const lookupAuthMemberLiked = (memberId: T, targetRefId: string = '$_id') => {
   return {
